9585009e48a76bd3b600f71233df3eb7413543c0
Achmad Fauzan Alif Fitrah <fznfitrah@gmail.com>
2025-11-10 07:15:29 +0700

    build: adding packages.txt

--- streamlit_app.py
+++
@@ -1,7 +0,0 @@
-import joblib
-
-model_rf = joblib.load('saved_models/random_forest_audio_multilabel.joblib')
-pca = joblib.load('saved_models/pca_selected_audio_multilabel.joblib')
-scaler = joblib.load('saved_models/scaler_audio_multilabel.joblib')
-
-
